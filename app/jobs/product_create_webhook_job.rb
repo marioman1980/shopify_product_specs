97278488fc90505c0b8f00319441f9cd5fb5b357
James <james@jkamradcliffe.net>
2018-12-07 18:05:28 +0000

    Successful metafield creation

--- app/jobs/product_create_webhook_job.rb
+++ app/jobs/product_create_webhook_job.rb
@@ -3,8 +3,13 @@ class ProductCreateWebhookJob < ActiveJob::Base
     shop = Shop.find_by(shopify_domain: shop_domain)
 
     shop.with_shopify_session do
-    	binding.pry
     	@product = ShopifyAPI::Product.find(webhook[:id])
+			@product.add_metafield(ShopifyAPI::Metafield.new({
+				:namespace => 'guitars',
+				:key => 'author',
+				:value => 'Kurt Vonnegut',
+				:value_type => 'string'
+			}))      	
     end
   end
 end
